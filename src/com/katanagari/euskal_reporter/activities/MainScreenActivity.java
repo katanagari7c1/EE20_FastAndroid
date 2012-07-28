2627cb1355ea70b870d928e007c19b5c3c318a37
Javi Armendáriz <katanagari@217.85-87-30.dynamic.clientes.euskaltel.es>
2012-07-28 04:09:36 +0200

    Crappy implementation of mail sender

--- src/com/katanagari/euskal_reporter/activities/MainScreenActivity.java
+++ src/com/katanagari/euskal_reporter/activities/MainScreenActivity.java
@@ -2,9 +2,12 @@ package com.katanagari.euskal_reporter.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.katanagari.euskal_reporter.R;
+import com.katanagari.euskal_reporter.classes.MailHelper;
 
 public class MainScreenActivity extends Activity {
 
@@ -12,11 +15,37 @@ public class MainScreenActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
+        
+        this.setSubmitButtonAction();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_screen, menu);
-        return true;
-    }
+//  @Override
+//  public boolean onCreateOptionsMenu(Menu menu) {
+//      getMenuInflater().inflate(R.menu.main_screen, menu);
+//      return true;
+//  }
+    
+	private void setSubmitButtonAction() {
+		Button submitButton = (Button)findViewById(R.id.submit_button);
+		
+		submitButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				onSubmitButtonPressed();
+			}
+		});
+	}
+	
+	
+	private void onSubmitButtonPressed(){
+		//Check if form has the minimum amount of data required
+		//If so, send it using a mail helper object
+		MailHelper helper = new MailHelper();
+		try {
+			helper.send();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
