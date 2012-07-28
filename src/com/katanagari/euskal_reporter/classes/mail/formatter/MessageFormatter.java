87fea7c07aebc97c445a524e60de8cc6a2447d5e
Javi Armendáriz <katanagari@217.85-87-30.dynamic.clientes.euskaltel.es>
2012-07-28 23:41:31 +0200

    Report will contain the path of the image to be sent. At least it works with mocked images

--- src/com/katanagari/euskal_reporter/classes/mail/formatter/MessageFormatter.java
+++ src/com/katanagari/euskal_reporter/classes/mail/formatter/MessageFormatter.java
@@ -1,7 +1,14 @@
 package com.katanagari.euskal_reporter.classes.mail.formatter;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import android.content.res.Resources;
 
@@ -19,14 +26,37 @@ public class MessageFormatter {
 
 			String subject = resources.getString(R.string.subject, report.getCategory());
 			String messageBody = resources.getString(R.string.message_body_head, report.getCategory());
+			Multipart multipart = new MimeMultipart();
 			
 			if (report.getDescription().length() > 0) {
 				messageBody += resources.getString(R.string.message_body_description, report.getDescription());
 			}
 			
-			//TODO: Photos missing!
+			if (report.getPhotoPath().length() > 0) {
+				messageBody += resources.getString(R.string.message_body_photo);
+				this.insertPhotoIntoEmailAsMultipart(report, multipart);
+			}	
 			
 			message.setSubject(subject);
-			message.setContent(messageBody, "text/plain");
+			this.insertContentIntoEmailAsMultipart(messageBody, multipart);
+			
+			message.setContent(multipart);
+	}
+
+	private void insertPhotoIntoEmailAsMultipart(Report report,
+			Multipart multipart) throws MessagingException {
+		BodyPart messageBodyPart = new MimeBodyPart();
+		DataSource source = new FileDataSource(report.getPhotoPath());
+		messageBodyPart.setDataHandler(new DataHandler(source));
+		messageBodyPart.setFileName(report.getPhotoPath());
+		
+		multipart.addBodyPart(messageBodyPart);
+	}
+
+	private void insertContentIntoEmailAsMultipart(String messageBody,
+			Multipart multipart) throws MessagingException {
+		BodyPart messageBodyPart = new MimeBodyPart();
+		messageBodyPart.setText(messageBody);
+		multipart.addBodyPart(messageBodyPart);
 	}
 }
